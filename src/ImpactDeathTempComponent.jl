6b5b87970f1252dac39b632f023d47c41109e97f
Valeri Vasquez <vnvasquez@berkeley.edu>
2017-01-19 23:28:12 -0800

    refine code in impactdeathtemp component

--- src/ImpactDeathTempComponent.jl
+++ src/ImpactDeathTempComponent.jl
@@ -9,12 +9,12 @@ using Mimi
     area           = Parameter(index=[time,regions])
     temp           = Parameter(index=[time,regions])
 
-    betaconstant1  = Parameter()
+    betaconstant   = Parameter()
+
     gammagdppc1    = Parameter()
     gammapopop1    = Parameter()
     gammameantemp1 = Parameter()
 
-    betaconstant2  = Parameter()
     gammagdppc2    = Parameter()
     gammapopop2    = Parameter()
     gammameantemp2 = Parameter()
@@ -24,6 +24,10 @@ using Mimi
     morttempeffect1 = Variable(index=[time,regions])
     morttempeffect2 = Variable(index=[time,regions])
     morttemp        = Variable(index=[time,regions])
+
+    dead            = Variable(index=[time,regions])
+    deadcost        = Variable(index=[time,regions])
+
 end
 
 
@@ -34,13 +38,21 @@ function run_timestep(s::impactdeathtemp, t::Int)
 
     if t>1
       for r in d.regions
+
+        # Question: why is ypc calculated separately in so many different components rather than just calculated once in socioeconomic?
+        v.ypc = p.income[t, r] / p.population[t, r] * 1000.0
+
         v.popop = p.populationin1[t, r] / p.area[t, r]
 
-        v.morttempeffect1 = p.betaconstant1 * exp(p.gammagdppc1 * p.ypc[r, t] + p.gammapopop1 * v.popop + p.gammameantemp1 * p.temp[r, t - 7])
+        v.morttempeffect1 = p.betaconstant * exp(p.gammagdppc1 * p.ypc[r, t] + p.gammapopop1 * v.popop + p.gammameantemp1 * p.temp[r, t - 7])
 
-        v.morttempeffect2 = p.betaconstant2 * exp(p.gammagdppc2 * p.ypc[r, t] + p.gammapopop2 * v.popop + p.gammameantemp2 * p.temp[r, t - 7])
+        v.morttempeffect2 = p.betaconstant * exp(p.gammagdppc2 * p.ypc[r, t] + p.gammapopop2 * v.popop + p.gammameantemp2 * p.temp[r, t - 7])
 
-        v.morttemp = (v.morttempeffect1 * p.temp[t, r] + v.morttempeffect2 * p.temp[t, r]^2) * p.population[t, r] * 1e6 / 100000.
+        # Using CIL data, this amounts to the change in mortality rate from a baseline of 2001-2010
+        v.dead = (v.morttempeffect1 * p.temp[t, r] + v.morttempeffect2 * p.temp[t, r]^2) * p.population[t, r] * 1e6 / 100000.
+
+        v.deadcost[t, r] = p.vsl[t, r] * v.dead[t, r] / 1000000000.0
+
       end
     end
 
