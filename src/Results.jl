fdd9e87a347613011d5e8859cb813143f6a5c350
Valeri Vasquez <vnvasquez@berkeley.edu>
2017-01-20 16:43:58 -0800

    create testrun to view results

---
+++ src/Results.jl
@@ -0,0 +1,8 @@
+#Load function to construct model
+include("fund.jl")
+
+#Create model for test run
+testrun = zeros(length(1950:3000));
+
+#Results
+CILcoeff_results = DataFrame(MortRate=testrun[:impactdeathtemp, :morttempeffect], DeadTot=testrun[:impactdeathtemp, :dead], CostTot=testrun[:impactdeathtemp, :deadcost])
