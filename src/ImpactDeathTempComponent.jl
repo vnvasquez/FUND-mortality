4a93a6d3fed26e90951f323a69d6338395a0d5c3
Valeri Vasquez <vnvasquez@berkeley.edu>
2017-02-03 13:58:28 -0800

    adjust vsl component to include low vs. high income values

--- src/ImpactDeathTempComponent.jl
+++ src/ImpactDeathTempComponent.jl
@@ -9,7 +9,8 @@ using Mimi
     population     = Parameter(index=[time,regions])
     income         = Parameter(index=[time,regions])
     area           = Parameter(index=[time,regions])
-    temp           = Parameter(index=[time])
+    temp           = Parameter(index=[time,regions])
+    funddead       = Parameter(index=[time,regions])
 
     betaconstant   = Parameter(index=[regions])
 
@@ -25,7 +26,6 @@ using Mimi
     logypc             = Variable(index=[time,regions])
     logpopop           = Variable(index=[time,regions])
     morttempeffect     = Variable(index=[time,regions])
-    funddead           = Variable(index=[time,regions])
     gcpdead            = Variable(index=[time,regions])
     dead               = Variable(index=[time,regions])
     deadcost           = Variable(index=[time,regions])
@@ -47,22 +47,28 @@ function run_timestep(s::impactdeathtemp, t::Int)
         v.logpopop[t, r] = log(p.populationin1[t, r] / p.area[t, r])
 
         # Using CIL data, this amounts to the change in mortality rate from a baseline of 2001-2010
-        v.morttempeffect[t, r] = p.betaconstant[r] + (p.gammatemp1[r] * p.temp[t - 7]) + (p.gammatemp2[r] * (p.temp[t - 7])^2) +
-                                (p.gammagdppc1[r] * p.temp[t - 7] * p.logypc[t, r]) + (p.gammagdppc2[r] * (p.temp[t - 7])^2 * p.logypc[t, r]) +
-                                (p.gammapopop1[r] * (p.temp[t - 7]) * v.logpopop[t, r]) + (p.gammapopop2[r] * (p.temp[t - 7])^2 * v.logpopop[t, r])
+        v.morttempeffect[t, r] = p.betaconstant[r] + (p.gammatemp1[r] * p.temp[t - 7, r]) + (p.gammatemp2[r] * (p.temp[t - 7, r])^2) +
+                                (p.gammagdppc1[r] * p.temp[t - 7, r] * p.logypc[t, r]) + (p.gammagdppc2[r] * (p.temp[t - 7, r])^2 * p.logypc[t, r]) +
+                                (p.gammapopop1[r] * (p.temp[t - 7, r]) * v.logpopop[t, r]) + (p.gammapopop2[r] * (p.temp[t - 7, r])^2 * v.logpopop[t, r])
 
         # Calculate number dead
-        v.gcpdead[t, r] = v.morttempeffect[r] * p.population[t, r]
+        v.gcpdead[t, r] = v.morttempeffect[t,r] * p.population[t, r]
 
         if t>=8
+
           for r in d.regions
-            v.dead[t, r] = (v.gcpdead[t, r] - v.gcpdead[8, r]) + v.funddead[8, r]
+
+            v.dead[t, r] = (v.gcpdead[t, r] - v.gcpdead[8, r]) + p.funddead[8, r]
+
           end
 
         # Cost
         v.deadcost[t, r] = p.vsl[t, r] * v.dead[t, r] / 1000000000.0
 
       end
+
     end
+
   end
+
 end
--- src/VslVmorbComponent.jl
+++ src/VslVmorbComponent.jl
@@ -3,18 +3,24 @@ using Mimi
 @defcomp vslvmorb begin
     regions = Index()
 
-    vsl = Variable(index=[time,regions])
-    vmorb = Variable(index=[time,regions])
+    vsl         = Variable(index=[time,regions])
+    vmorb       = Variable(index=[time,regions])
+    ypc         = Variable(index=[time,regions])
 
-    population = Parameter(index=[time,regions])
-    income = Parameter(index=[time,regions])
+    population  = Parameter(index=[time,regions])
+    income      = Parameter(index=[time,regions])
 
-    vslbm = Parameter()
-    vslel = Parameter()
-    vmorbbm = Parameter()
-    vmorbel = Parameter()
-    vslypc0 = Parameter()
-    vmorbypc0 = Parameter()
+    vslbm       = Parameter()
+    vslypc0     = Parameter()
+    #vslel       = Parameter()
+    vslel_high  = Parameter()
+    vslel_mid   = Parameter()
+    #vslel_low   = Parameter()
+
+    #vmorbbm     = Parameter()
+    #vmorbel     = Parameter()
+    #vmorbypc0   = Parameter()
+
 end
 
 function run_timestep(s::vslvmorb, t::Int)
@@ -24,11 +30,19 @@ function run_timestep(s::vslvmorb, t::Int)
 
     if t>1
         for r in d.regions
-            ypc = p.income[t, r] / p.population[t, r] * 1000.0
 
-            v.vsl[t, r] = p.vslbm * (ypc / p.vslypc0)^p.vslel
+            # Note for discussion: added ".v" ahead of ypc, and included as variable above.
+            v.ypc[t,r] = p.income[t, r] / p.population[t, r] * 1000.0
 
-            v.vmorb[t, r] = p.vmorbbm * (ypc / p.vmorbypc0)^p.vmorbel
+            # Explicit VSL values of 1.0 and 1.5.
+            # Q: include VSL of 0.5 too? Miguel et al.
+            if v.ypc < p.vslypc0
+              v.vsl[t, r] = p.vslbm * (v.ypc[t,r] / p.vslypc0)^p.vslel_high
+            else
+              v.vsl[t, r] = p.vslbm * (v.ypc[t,r] / p.vslypc0)^p.vslel_mid
+            end
+
+            #v.vmorb[t, r] = p.vmorbbm * (ypc / p.vmorbypc0)^p.vmorbel
         end
     end
 end
